e0376320c4a139c63c22ef3dfb7d53810802923c
codlab <codlabtech@gmail.com>
2022-01-09 21:52:09 +0100

    remove type

--- src/exchanges/defs/index.ts
+++ src/exchanges/defs/index.ts
@@ -1,9 +1,9 @@
 export { AccountBalance } from './AccountBalance';
 export { AccountBalanceValue } from './AccountBalanceValue';
-export type { Balances } from './Balances';
+export { Balances } from './Balances';
 export { CurrencyLimit } from './CurrencyLimit';
-export type { Devise } from './Devise';
+export { Devise } from './Devise';
 export { DeviseNames } from './DeviseNames';
-export type { OrderType } from './OrderType';
+export { OrderType } from './OrderType';
 export { ShortOrder } from './ShortOrder';
 export { Ticker } from './Ticker';
